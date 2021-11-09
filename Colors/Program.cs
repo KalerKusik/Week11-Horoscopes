4f1094df043d0b1bbe84d886ff178ce28fa6d9dd
KalerKusik <karel.kuusik1@gmail.com>
2021-11-09 13:38:34 +0200

    Add project files.

---
+++ Colors/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace Colors
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your favorite color:");
+            string userSign = Console.ReadLine();
+            Console.WriteLine(GetColors(userSign));
+        }
+        public static string[] ReadColors()
+        {
+            string filePath = @"C:\Users\admin\samples\personality.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);
+            return dataFromFile;
+        }
+
+        public static string GetColors(string userInput)
+        {
+            string[] colors = ReadColors();
+            string favColors = "";
+
+            foreach (string element in colors)
+            {
+                if (element.Contains(userInput))
+                {
+                    favColors = element;
+                }
+            }
+
+            return favColors;
+        }
+    }
+}
